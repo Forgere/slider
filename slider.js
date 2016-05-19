3e220f5ef68cd4df893d631d4923063d0fbb72c2
forgere <573596314@qq.com>
2016-05-19 17:44:11 +0800

    slider 消除

--- slider.js
+++ slider.js
@@ -10,88 +10,55 @@
                 width = $lis.eq(0).width(),
                 numberaccout = number || 4,
                 gundongaccout = gundong || 1,
+                auto_status = status || true,
                 mf=parseInt($ul.css('margin-left'));
             $(a).css({'width':width*numberaccout});
-            timemachine=setInterval(function(){
-                if(-mf<($lis.length-numberaccout)*width){
-                    mf = mf - width;
-                }else{
-                    mf = 0;
+                if(auto_status){
+                    timemachine=setInterval(function(){
+                        if(-mf<($lis.length-numberaccout)*width){
+                            mf = mf - width;
+                        }else{
+                            mf = 0;
+                        }
+                        $ul.stop().animate({
+                                'margin-left': mf +'px'
+                            },
+                            'slow');
+                        },3000
+                    );
                 }
-                $ul.animate({
-                        'margin-left': mf +'px'
-                    },
-                    'slow',
-
-                    function(){
-                        if (status === true) {
-                        //此处保证能循环轮播
-                        //将已经轮播过的节点的第一张图片添加到末尾的位置
-                        //再将margin-left重置为0px;
-                        //这样就能一直的循环下去.
-                        $ul.css({'margin-left': 0 +'px'}).
-                            children('li').
-                            last().
-                            after(
-                                $ul.children('li').slice(0, gundongaccout)
-                            );
-                        }
-                    });
-                },3000
-            );
-            // $('.slider').hover(function(){
-            //     window.clearInterval(timemachine);
-            // });
-            // $('.slider').mouseleave(function(){
-            //     silde.init(a,number,gundong,status);
-            // });
+            // 接触时暂停
+            $('.slider').hover(function(){
+                window.clearInterval(timemachine);
+            });
+            // 鼠标移开
+            $('.slider').mouseleave(function(){
+                silde.init(a,number,gundong,auto_status);
+            });
             $('.arrow-right').click(function(){
-                    $ul.animate({
-                            'margin-left':'-'+ gundongaccout*width +'px'
-                        },
-                        'slow',
-                        function(){
-                            //此处保证能循环轮播
-                            //将已经轮播过的节点的第一张图片添加到末尾的位置
-                            //再将margin-left重置为0px;
-                            //这样就能一直的循环下去.
-                            if (status) {
-                            $ul.
-                                children('li').
-                                last().
-                                after(
-                                    $ul.children('li').slice(0, gundongaccout)
-                                );
-                            $ul.css({'margin-left':'-'+ gundongaccout*width +'px'});
-                        }
-                        });
-                    }
-                );
-            $('.arrow-left').click(function(){
-                $ul.animate({
-                        'margin-left':0 +'px'
+                if(-mf>($lis.length-numberaccout-1)*width){
+                    return;
+                }
+                mf -= width;
+                $ul.stop().animate({
+                        'margin-left':mf +'px'
                     },
-                    'slow',
-                    function(){
-                        //此处保证能循环轮播
-                        //将已经轮播过的节点的第一张图片添加到末尾的位置
-                        //再将margin-left重置为0px;
-                        //这样就能一直的循环下去.
-                         if (status) {
-                        $ul.
-                            children('li').
-                            first().
-                            before(
-                                $ul.children('li').slice(-1)
-                            );
-                        $ul.css({'margin-left':'-'+ gundongaccout*width +'px'});
-                    }
-                    });
+                    'fast');
+                });
+            $('.arrow-left').click(function(){
+                if(mf === 0){
+                    return;
+                }
+                mf += width;
+                $ul.stop().animate({
+                        'margin-left':mf +'px'
+                    },
+                    'fast');
                 }
             );
         }
     };
     $(function(){
-        silde.init('.slider',3,1,false);
+        silde.init('.slider',3,1,true);
     });
 })(jQuery);
