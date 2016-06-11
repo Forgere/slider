c3446cd4ca15c5e6edea4e3a227135e36e06341d
forgere <573596314@qq.com>
2016-06-11 13:57:14 +0800

    删除图片成功，接下来保存数据 left right是应用数据

--- slider3.js
+++ slider3.js
@@ -1,5 +1,5 @@
-(function($) {
-    var Slider = function() {
+(function ($) {
+    var Slider = function () {
         //  保存对象
         var _ = this;
 
@@ -12,7 +12,7 @@
             items: '>ul',
             item: '>li',
             autoplay: true,
-            loop : false,
+            loop: false,
             autochange: true,
             duration: 3000,
             arrows: true,
@@ -23,12 +23,12 @@
             array: [],
             cache: [],
             easing: 'swing',
-            lazyload:true,
+            lazyload: true,
             loading: 'loading.gif',
             fadeIn: false
         };
 
-        _.init = function(el, o) {
+        _.init = function (el, o) {
             //  信息合并
             _.o = $.extend(_.o, o);
             _.el = el;
@@ -43,12 +43,12 @@
             _.maxI = _.o.number;
             //初始加赞图片
             _.o.array = romoteArray.slice(_.i, _.i + _.o.number);
-            if(_.o.lazyload){
+            if (_.o.lazyload) {
                 for (var i = 0; i < _.o.number; i++) {
                     _.addImage(_.o.array);
                     _.i++;
                 }
-            }else{
+            } else {
                 for (var i = 0; i < _.o.number; i++) {
                     $("<li style='left:" + _.i * _.parentW / _.o.number + 'px' + "'><img src=" + _.o.array[0] + " style='width:" + _.liWidth + ";'></li>").appendTo(_.ul);
                     _.i++;
@@ -59,12 +59,12 @@
             //高度
             _.ul.height('100%');
             //  Autoslide
-            _.o.autoplay && setTimeout(function() {
+            _.o.autoplay && setTimeout(function () {
                 if (_.o.duration | 0) {
-                    if(_.o.autoplay){
+                    if (_.o.autoplay) {
                         _.play();
                     }
-                    _.el.on('mouseover mouseout', function(e) {
+                    _.el.on('mouseover mouseout', function (e) {
                         _.stop();
                         _.o.autoplay && e.type == 'mouseout' && _.play();
                     });
@@ -74,51 +74,59 @@
             o.dots && nav('dot');
 
             //  Arrows support
-            if(o.arrows){
+            if (o.arrows) {
                 nav('arrow');
             }
             //  Patch for fluid-width sliders. Screw those guys.
             if (o.autochange) {
-                $(window).resize(function() {
+                $(window).resize(function () {
                     var sliderWidth = Math.floor(parseInt(el.parent().css('width')) / _.o.number) * _.o.number;
                     _.r && clearTimeout(_.r);
 
-                    _.r = setTimeout(function() {
+                    _.r = setTimeout(function () {
                         el.width(sliderWidth);
-                        el.find('img').width(sliderWidth/_.o.number);
+                        el.find('img').width(sliderWidth / _.o.number);
                         var liWidth = el.find(_.o.items).children('li').eq(0).width();
                         for (var i = 0; i < el.find(_.o.items).children('li').length; i++) {
-                            el.find(_.o.items).children('li').eq(i).css('left',liWidth * i);
+                            el.find(_.o.items).children('li').eq(i).css('left', liWidth * i);
                         }
-                        el.find(_.o.items).css('left', (_.o.number-_.i)*liWidth);
+                        el.find(_.o.items).css('left', (_.o.number - _.i) * liWidth);
                     }, 50);
                 }).resize();
-            };
+            }
+            if(_.o.savenumber){
+                var protectTime = setInterval(
+                    function(){
+                    _.protectMemory();
+                    }
+                    ,3000);
+            }
             return _;
         };
 
         //  根据_.i移动ul
-        _.to = function(index, callback) {
+        _.to = function (index, callback) {
             if (_.t) {
                 _.stop();
                 _.play();
-                    }
+            }
             var o = _.o,
                 el = _.el,
                 ul = _.ul,
                 li = _.li,
                 current = _.i,
                 target = romoteArray[index];
-                console.log(_.i);
-                console.log(_.maxI);
+            console.log(_.i);
+            console.log(_.maxI);
             //  slider到达边缘条件
-            if ((romoteArray.length+1 === _.i) && o.loop === false) {
+            if ((romoteArray.length + 1 === _.i) && o.loop === false) {
                 _.i = index;
                 return;
             }
             if (index < _.o.number && o.loop === false) {
                 _.i = _.o.number;
-                return;}
+                return;
+            }
 
             //  无线循环
             // if (romoteArray.length === _.i) index = 0;
@@ -128,13 +136,15 @@
             var speed = callback ? 5 : o.speed | 0,
                 easing = o.easing,
                 obj = {};
-                // obj = {height: target.outerHeight()};
+            // obj = {height: target.outerHeight()};
 
             if (!ul.queue('fx').length) {
                 //  Handle those pesky dots
                 // el.find('.dot').eq(index).addClass('active').siblings().removeClass('active');
 
-                el.animate(obj, speed, easing) && ul.animate($.extend({left: (_.o.number-index)*Math.floor(parseInt(el.parent().css('width')) / _.o.number) * _.o.number/_.o.number}, obj), speed, easing, function(data) {
+                el.animate(obj, speed, easing) && ul.animate($.extend({
+                    left: (_.o.number - index) * Math.floor(parseInt(el.parent().css('width')) / _.o.number) * _.o.number / _.o.number
+                }, obj), speed, easing, function (data) {
                     _.i = index;
                     _.maxI = (index > _.maxI) ? index : _.maxI;
                 });
@@ -142,46 +152,48 @@
         };
 
         //  自动增加index
-        _.play = function() {
-            _.t = setInterval(function() {
+        _.play = function () {
+            _.t = setInterval(function () {
                 _.to(_.i + 1);
             }, _.o.duration | 0);
         };
 
         //  Stop
-        _.stop = function() {
+        _.stop = function () {
             _.t = clearInterval(_.t);
             return _;
         };
 
         //  右箭头
-        _.next = function() {
-            if ( romoteArray.length === _.i) return;
-            _.getArray(_.i,_.i + 1);
+        _.next = function () {
+            if (romoteArray.length === _.i) return;
+            _.getArray(_.i, _.i + 1);
             _.addImage(_.o.array);
             return _.stop().to(_.i + 1);
         };
         //  左箭头
-        _.prev = function() {
-            if ( _.o.number === _.i) return;
+        _.prev = function () {
+            if (_.o.number === _.i) return;
             return _.stop().to(_.i - 1);
         };
         //获取数据
-        _.getArray = function(from,to){
+        _.getArray = function (from, to) {
             if (to <= _.maxI) {
                 _.o.array = [];
-            }else{
+            } else {
                 _.o.array = romoteArray.slice(from, to);
             }
             return _.o.array;
         };
         //加入图片
-        _.addImage = function(array){
-            if (_.o.array.length === 0) {return;}
+        _.addImage = function (array) {
+            if (_.o.array.length === 0) {
+                return;
+            }
             //lazyload support
             if (_.o.lazyload) {
                 _.lazyload(array);
-            }else{
+            } else {
                 $("<li style='left:" + _.i * Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) + 'px' + "'><img src=" + array[0] + " style='width:" + Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) + ";'></li>").appendTo(_.ul);
                 array.shift();
             }
@@ -190,45 +202,64 @@
         function nav(name, html) {
             if (name == 'dot') {
                 html = '<ol class="dots">';
-                    $.each(_.li, function(index) {
-                        html += '<li class="' + (index == _.i ? name + ' active' : name) + '">' + ++index + '</li>';
-                    });
+                $.each(_.li, function (index) {
+                    html += '<li class="' + (index == _.i ? name + ' active' : name) + '">' + ++index + '</li>';
+                });
                 html += '</ol>';
             } else {
                 html = '<div class="';
                 html = html + name + 's">' + html + name + ' prev">' + _.o.prev + '</div>' + html + name + ' next">' + _.o.next + '</div></div>';
             }
 
-            _.el.addClass('has-' + name + 's').append(html).find('.' + name).click(function() {
+            _.el.addClass('has-' + name + 's').append(html).find('.' + name).click(function () {
                 var me = $(this);
                 me.hasClass('dot') ? _.stop().to(me.index()) : me.hasClass('prev') ? _.prev() : _.next();
             });
         }
         //lazyload
-        _.lazyload = function(array){
-            var creatImg =  $("<li style='left:" + _.i * Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) + 'px' + "'><img src="+_.o.loading+" style='width:" + Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) + ";'></li>");
+        _.lazyload = function (array) {
+            var creatImg = $("<li style='left:" + _.i * Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) + 'px' + "'><img src=" + _.o.loading + " style='width:" + Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) + ";'></li>");
             creatImg.appendTo(_.ul);
             var tmpimg = $("<img src=" + array[0] + ">");
-            tmpimg.ready(function(){
-                setTimeout(function(){
+            tmpimg.ready(function () {
+                setTimeout(function () {
                     //fadeIn
-                    if(_.o.fadeIn){
+                    if (_.o.fadeIn) {
                         creatImg.find('img').attr('src', tmpimg.attr('src')).hide().fadeIn('slow');
-                    }else{
+                    } else {
                         creatImg.find('img').attr('src', tmpimg.attr('src'));
                     }
-                },1000);
+                }, 1000);
             });
             array.shift();
+        };
+        //保护内存
+        _.protectMemory = function(){
+            //留下的图片的index 从0开始
+            var protectededFirst = _.i - (_.o.number + _.o.savenumber);
+            var protectededLast = _.i + _.o.savenumber - 1;
+            //限制修正这两个index
+            protectededFirst = (protectededFirst < 0) ? 0 : protectededFirst;
+            protectededLast = (protectededLast > _.maxI) ? _.maxI : protectededLast;
+            //删除其他图片
+            for (var i = 0; i < _.el.find(_.o.items).children('li').length; i++) {
+                var liEachLeft = parseInt(_.el.find(_.o.items).children('li').eq(i).css('left'));
+                var liEachWidth = Math.floor(parseInt(_.el.parent().css('width')) / _.o.number) * _.o.number / _.o.number;
+                var eachImageIndex = liEachLeft / liEachWidth;
+                console.log(eachImageIndex);
+                if(eachImageIndex < protectededFirst || eachImageIndex > protectededLast){
+                    _.el.find(_.o.items).children('li').eq(i).remove();
+                }
+            }
         };
     };
 
     //  Create a jQuery plugin
-    $.fn.silder = function(o) {
+    $.fn.silder = function (o) {
         var len = this.length;
 
         //  Enable multiple-slider support
-        return this.each(function(index) {
+        return this.each(function (index) {
             //  Cache a copy of $(this), so it
             var me = $(this),
                 key = 'unslider' + (len > 1 ? '-' + ++index : ''),
@@ -241,7 +272,7 @@
 
 })(jQuery);
 $(function () {
-    romoteArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg','10.jpg','11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg','20.jpg'];
+    romoteArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
     $('.slider').silder({
         number: 3, //图数量
         savenumber: 3, //显示前后保存的数据
@@ -251,7 +282,7 @@ $(function () {
         duration: 3000, //自动播放时延迟
         speed: 200, //箭头滚动速度
         autospeed: 'slow', //自动播放速度
-        lazyload:true,  //是否开启lazyload
+        lazyload: true, //是否开启lazyload
         loading: 'loading.gif', //加载中的图片
         fadeIn: true, //开启fadeIn滚动特效
     });
